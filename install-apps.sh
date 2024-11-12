f300dbed054f2229c4eec474268e3a85255f0213
Stephen Luc <stephenluc@users.noreply.github.com>
2024-11-11 23:07:10 -0500

    Create install-apps.sh

---
+++ install-apps.sh
@@ -0,0 +1,14 @@
+# Personal
+brew install --cask arc
+brew install --cask flycut
+brew install --cask rectangle
+brew install --cask iterm2
+brew install --cask raycast
+brew install --cask keeper-password-manager
+brew install --cask messenger
+
+# Work
+brew install --cask cursor
+brew install --cask slack
+brew install --cask mongodb-compass
+brew install --cask postman
