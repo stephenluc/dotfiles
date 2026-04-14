4a15f7553e05e07bffa875530ce2c6295c0bba82
stephenluc <stephenluc96@gmail.com>
2026-04-14 12:03:57 -0400

    add tmux-navigator keybindings and vim-sleuth

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -8,6 +8,17 @@ return {
       require("nvim-autopairs").setup({})
     end,
   },
-  { "christoomey/vim-tmux-navigator" },
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
+    },
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+  },
   { "tpope/vim-sleuth" },
 }
